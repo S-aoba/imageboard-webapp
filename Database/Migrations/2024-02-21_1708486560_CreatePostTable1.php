e5041ad8b665910f546d04d6ee00159509fda13a
S-aoba <writer_fairy_road@yahoo.co.jp>
2024-02-21 13:20:32 +0900

    🗃️  add: posts table schema

---
+++ Database/Migrations/2024-02-21_1708486560_CreatePostTable1.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Migrations;
+
+use Database\SchemaMigration;
+
+class CreatePostTable1 implements SchemaMigration
+{
+    public function up(): array
+    {
+        // マイグレーションロジックをここに追加してください
+        return [
+            "CREATE TABLE IF NOT EXISTS posts (
+                id INT PRIMARY KEY AUTO_INCREMENT,
+                reply_to_id INT,
+                subject VARCHAR(255),
+                content TEXT,
+                created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+            )"
+        ];
+    }
+
+    public function down(): array
+    {
+        // ロールバックロジックを追加してください
+        return [
+            "DROP TABLE IF EXISTS posts"
+        ];
+    }
+}
