556eae7c2c20043e6e8f04eba5dd974d64b54d48
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-01-27 22:13:33 +0900

    Change bootstrap css to tailwind css

--- Views/layout/header.php
+++ Views/layout/header.php
@@ -1,14 +1,14 @@
 <!doctype html>
-<html lang="en">
+<html lang="ja" class="h-screen">
     <head>
         <!-- Required meta tags -->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Bootstrap CSS -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+        <!-- Tailwind CSS -->
+        <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
 
         <title>My Computer Parts Store</title>
     </head>
-    <body>
-        <main class="container mt-5 mb-5">
+    <body class="w-full h-full bg-green-50">
+        <main class="bg-red-50 w-full">
